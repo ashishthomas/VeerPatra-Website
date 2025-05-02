370d0706ddabf079a97cf5335d55f054dfa770e8
hmeshram21 <harsh.meshram.ext@holcim.com>
2025-05-02 18:49:59 +0530

    Commit changes to Media.jsx

--- src/pages/Media.jsx
+++ src/pages/Media.jsx
@@ -6,10 +6,10 @@ import PageLayout from "../Components/PageLayout";
 const Media = () => {
   return (
     <PageLayout>
-      <h1 className="font-semibold text-center mt-[12vh] mb-6 text-4xl">
+      <h1 className="font-semibold text-center mt-[12vh] mb-6 text-2xl sm:text-3xl md:text-4xl">
         Media Gallery
       </h1>
-      <div className="flex justify-center flex-wrap gap-10 mb-[9vh]">
+      <div className="flex justify-center flex-wrap gap-4 sm:gap-6 md:gap-10 px-4 sm:px-8 max-w-screen-xl mx-auto mb-[9vh]">
         {letters.map((letter) => (
           <LetterCard key={letter.imageId} letterData={letter} />
         ))}
