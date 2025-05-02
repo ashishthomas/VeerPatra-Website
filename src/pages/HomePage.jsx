905f16798a4508f0f36f4238cab608133a2a6174
hmeshram21 <harsh.meshram.ext@holcim.com>
2025-05-02 18:36:32 +0530

    Commit only HomePage.jsx changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import Footer from "./Footer";
 import Testimonial from "./Testimonial";
 import Faq from "./Faq";
-import Navbar from "../Components/Navbar";
 import Gallery from "./Gallery";
 import Hero from "../Components/Hero";
-import { Outlet } from "react-router-dom";
 import PageLayout from "../Components/PageLayout";
 
 function HomePage() {
