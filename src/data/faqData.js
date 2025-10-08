a2fc2ebbf8799270c03a360db81b292a9e3820f1
ashish <ashishthomas1995@gmail.com>
2025-10-08 10:17:21 +0530

    updated Faq Component and moved the object data into the seperate data folder

---
+++ src/data/faqData.js
@@ -0,0 +1,22 @@
+export const faqData = [
+  {
+    title: "General settings",
+    content:
+      "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feAliquam eget maximus est, id dignissim quam.",
+  },
+  {
+    title: "Users",
+    content:
+      "Donec placerat, lectus sed mattis semper, neque lectus feugiat lectusvarius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.",
+  },
+  {
+    title: "Advanced settings",
+    content:
+      "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
+  },
+  {
+    title: "Personal data",
+    content:
+      "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
+  },
+];
--- src/pages/Faq.jsx
+++ src/pages/Faq.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import {
   Accordion,
   AccordionDetails,
@@ -5,28 +6,10 @@ import {
   Typography,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import React from "react";
+import { faqData } from "../data/faqData";
 
 function Faq() {
   const [expanded, setExpanded] = React.useState(false);
-  const faq = [
-    {
-      "General settings":
-        "Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feAliquam eget maximus est, id dignissim quam.",
-    },
-    {
-      Users:
-        " Donec placerat, lectus sed mattis semper, neque lectus feugiat lectusvarius pulvinar diam eros in elit. Pellentesque convallis laoreet laoreet.",
-    },
-    {
-      "Advanced settings":
-        "Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
-    },
-    {
-      "Personal data":
-        " Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit amet egestas eros, vitae egestas augue. Duis vel est augue.",
-    },
-  ];
 
   const handleChange = (panel) => (event, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -34,32 +17,28 @@ function Faq() {
 
   return (
     <div className="bg-gradient-to-r from-orange-400 via-white to-green-600 p-5 flex flex-col gap-4">
-      {faq.map((obj, ind) => {
-        const panelId = `panel${ind + 1}`;
+      {faqData.map((item, index) => {
+        const panelId = `panel${index + 1}`;
         return (
           <Accordion
             key={panelId}
             expanded={expanded === panelId}
             onChange={handleChange(panelId)}
           >
-            {Object.entries(obj).map(([title, content]) => (
-              <React.Fragment key={title}>
-                <AccordionSummary
-                  expandIcon={<ExpandMoreIcon />}
-                  aria-controls={`${panelId}-content`}
-                  id={`${panelId}-header`}
-                >
-                  <Typography variant="body1" className="w-[33%] flex-shrink-0">
-                    {title}
-                  </Typography>
-                </AccordionSummary>
-                {expanded === panelId && (
-                  <AccordionDetails>
-                    <Typography>{content}</Typography>
-                  </AccordionDetails>
-                )}
-              </React.Fragment>
-            ))}
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls={`${panelId}-content`}
+              id={`${panelId}-header`}
+            >
+              <Typography variant="body1" className="w-[33%] flex-shrink-0">
+                {item.title}
+              </Typography>
+            </AccordionSummary>
+            {expanded === panelId && (
+              <AccordionDetails>
+                <Typography>{item.content}</Typography>
+              </AccordionDetails>
+            )}
           </Accordion>
         );
       })}
